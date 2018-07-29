a554b2249d4333d29f061e6a450e18b91b69ffbe
captyn <captyn1@gmail.com>
2018-07-30 00:04:30 +0200

    d

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -14,8 +14,20 @@ class ArticleController extends Controller
      */
     public function index()
     {
-        return new Response('RKN v 9.1!');
+        return $this->render('index.html.twig');
     }
+
+
+    /*public function show($slug)
+    {
+        $articlebody = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+        $title = 'Tytuł Artykułu';
+        $date = '';
+        return $this->render('testshow.html.twig', [
+            'title' => $title,
+            'comments' => $articlebody,
+        ]);
+    }*/
 
     /**
      * @Route("/news/{slug}")
